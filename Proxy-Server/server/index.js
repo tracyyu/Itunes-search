a3dc770e08eb1692cb3bf35eac35b28ee1258d3e
Tracy Yu <tracyyu93@g.ucla.edu>
2019-07-12 10:02:57 -0700

    Update access controll origin

--- Proxy-Server/server/index.js
+++ Proxy-Server/server/index.js
@@ -21,7 +21,7 @@ server.use(express.static(path.join(__dirname, '../dist')));
  */
 
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Origin", "http://3.84.145.74:3001/");
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
