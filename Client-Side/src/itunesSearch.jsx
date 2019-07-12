7d92f0e316622f9b131c36df0d3668cf797567d7
Tracy Yu <tracyyu93@g.ucla.edu>
2019-07-12 11:46:52 -0700

    Update query to include all media

--- Client-Side/src/itunesSearch.jsx
+++ Client-Side/src/itunesSearch.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 import SearchBar from './components/searchBar/searchBar.jsx';
 import SearchList from './components/searchList/searchList.jsx';
--- Proxy-Server/helper/itunesSearch.js
+++ Proxy-Server/helper/itunesSearch.js
@@ -2,9 +2,48 @@
 
 const request = require('request');
 
-const getResultByTerm = (term, callback) => {
+const toCapitalize = (str) => {
+  return `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+}
+
+// Media options: movie, podcast, music, musicVideo, audiobook, shortFilm, tvShow, software, ebook, all
+const mediaOptions = ['All','Audiobook','eBook','Movie','Music','Music Video','Podcast','TV Show', 'Short Film', 'Software'];
+
+
+const getMediaType = (str) => {
+  if(str.indexOf(' ') === -1){
+    return str.toLowerCase();
+  }
+  const strArr = str.split(' ');
+  return `${strArr[0].toLowerCase()}${toCapitalize(strArr[1])}`;
+}
+
+const getKindType = (str) => {
+  if(typeof str !== 'string'){
+    return '';
+  }
+
+  if(str.indexOf(' ') !== -1){
+
+  }
+
+  if(str === 'tv'){
+    return 'TV';
+  } else if (str === 'feature'){
+    return '';
+  }
+
+  if(str.indexOf('-') === -1){
+    return capitalize(str);
+  }
+  const strArr = str.split('-');
+  return `${getKindType(strArr[0])} ${toCapitalize(strArr[1])}`.trim();
+}
+
+
+const getResultByTerm = (media, term, callback) => {
   let options = {
-    url: `https://itunes.apple.com/search?term=${term}`,
+    url: `https://itunes.apple.com/search?media=all&term=${query.split(' ').join('+')}`,
     headers: {
       'User-Agent': 'request',
     }
