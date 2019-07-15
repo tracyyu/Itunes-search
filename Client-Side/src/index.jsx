b44f5b4f8f4149179c4d8e576c9fc3261338e2ce
Tracy Yu <tracyyu93@g.ucla.edu>
2019-07-15 16:24:11 -0700

    Update syntax

--- Client-Side/src/index.jsx
+++ Client-Side/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ItunesSearch from './ItunesSearch.jsx';
 
-ReactDOM.render(<ItunesSearch />, document.getElementById('app'))
+ReactDOM.render(<ItunesSearch />, document.getElementById('app'));
